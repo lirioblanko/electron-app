26229c7c91a8f26d9c2a9e397f6630ee1f3aa5f0
liliya <goldliliya@mail.ru>
2025-03-04 20:33:59 +0300

    settings

--- src/main/index.js
+++ src/main/index.js
@@ -1,15 +1,48 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, screen, BrowserWindow, ipcMain, Menu, MenuItem } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 import fs from 'fs/promises'
+import { watch } from 'fs'
+
+const menuTemplate = [
+  {
+    label: 'MyApp',
+    submenu: [
+      { role: 'about' },
+      new MenuItem({
+        label: 'О программе',
+        click: () => {}
+      }),
+      new MenuItem({
+        type: 'separator'
+      }),
+      new MenuItem({
+        label: 'Закрыть',
+        click: () => {
+          app.quit()
+        }
+      })
+    ]
+  }
+]
+
+function createMenu() {
+  const menu = new Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(menu)
+}
 
 function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 900,
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+
+  let mainWindow = new BrowserWindow({
+    width: 800,
     height: 670,
+    minWidth: 400,
+    maxWidth: width,
+    maxHeight: height,
     show: false,
+    titleBarStyle: 'hidden',
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
@@ -18,47 +51,42 @@ function createWindow() {
     }
   })
 
+  // mainWindow.webContents.setWindowOpenHandler((details) => {
+  //   shell.openExternal(details.url)
+  //   return { action: 'deny' }
+  // })
+
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
 
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
-
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  mainWindow.on('close', () => {
+    mainWindow = null
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
   // IPC test
+  createWindow()
+  createMenu()
   ipcMain.on('ping', () => console.log('pong'))
 
-  createWindow()
-
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
   })
 })
 
